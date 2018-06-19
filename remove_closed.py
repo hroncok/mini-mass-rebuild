8b2ebc6a27e642206185686e3cc5a2fe326c0ca0
Miro Hrončok <miro@hroncok.cz>
2018-06-19 12:52:42 +0200

    Add helper script that helps remove closed builds from the pkg list

---
+++ remove_closed.py
@@ -0,0 +1,11 @@
+import pathlib
+import sys
+
+original = pathlib.Path(sys.argv[1])
+closed = pathlib.Path(sys.argv[2])
+new = pathlib.Path(sys.argv[3])
+
+originals = set(original.read_text().splitlines())
+closeds = set(closed.read_text().splitlines())
+news = originals - closeds
+new.write_text('\n'.join(sorted(news)))
