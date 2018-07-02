f974435b681bbba6060195226848631764d521ca
Miro Hrončok <miro@hroncok.cz>
2018-07-02 20:39:06 +0200

    Put commented "bump && commit && psuh" example to fedpkg-build.sh

--- fedpkg-build.sh
+++ fedpkg-build.sh
@@ -2,6 +2,9 @@ set -o pipefail  # make tee preserve the exit code
 fedpkg clone $1  2>&1 | tee ./${1}.log || exit $?
 
 cd $1
+  #rpmdev-bumpspec -c "Rebuilt for Python 3.7" *.spec | tee -a ../${1}.log
+  #git commit *.spec -m "Rebuilt for Python 3.7" | tee -a ../${1}.log
+  #git push
   fedpkg build --nowait --background 2>&1 | tee -a ../${1}.log
 cd -
 
