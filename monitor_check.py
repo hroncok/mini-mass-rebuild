213ea662cd4780af08e438b7fd8f1e0d3a34d54e
Miro Hrončok <miro@hroncok.cz>
2019-07-31 13:52:24 +0200

    3.8.0b3

--- monitor_check.py
+++ monitor_check.py
@@ -177,7 +177,7 @@ async def open_bz(package, build, status, browser_lock):
     summary = f"{package} fails to build with Python 3.8"
 
     description = dedent(f"""
-        {package} fails to build with Python 3.8.0b2.
+        {package} fails to build with Python 3.8.0b3.
 
         This report is automated and not very verbose, but we'll try to get back here with details.
 
