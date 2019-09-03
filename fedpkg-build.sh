107d01ce56697a3a41a40909ebce0019e6f92157
Miro Hrončok <miro@hroncok.cz>
2019-09-03 12:50:14 +0200

    Comment out the committing part

--- fedpkg-build.sh
+++ fedpkg-build.sh
@@ -2,11 +2,11 @@ set -o pipefail  # make tee preserve the exit code
 fedpkg clone $1  2>&1 | tee ./${1}.log || exit $?
 
 cd $1
-  if ! git show --name-only | grep -F "Python 3.8"; then
-    rpmdev-bumpspec -c "Rebuilt for Python 3.8" *.spec | tee -a ../${1}.log
-    git commit *.spec -m "Rebuilt for Python 3.8" | tee -a ../${1}.log
-    git push
-  fi
+  #if ! git show --name-only | grep -F "Python 3.8"; then
+  #  rpmdev-bumpspec -c "Rebuilt for Python 3.8" *.spec | tee -a ../${1}.log
+  #  git commit *.spec -m "Rebuilt for Python 3.8" | tee -a ../${1}.log
+  #  git push
+  #fi
   fedpkg build --nowait --background 2>&1 | tee -a ../${1}.log
 cd -
 
