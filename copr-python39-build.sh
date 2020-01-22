34982fc55efe6a0bbbb3a235c8aec9456916ee89
Miro Hrončok <miro@hroncok.cz>
2020-01-22 10:54:01 +0100

    Add Python3.9 copr rebuild script for parallel

---
+++ copr-python39-build.sh
@@ -0,0 +1,11 @@
+set -o pipefail  # make tee preserve the exit code
+fedpkg clone $1  2>&1 | tee ./${1}.log || exit $?
+
+cd $1
+  #rpmdev-bumpspec -c "Rebuilt for Python 3.9" *.spec | tee -a ../${1}.log
+  fedpkg srpm | tee -a ../${1}.log
+  # get API token from https://copr.fedorainfracloud.org/api
+  copr build --nowait @python/python3.9 *.src.rpm | tee -a ../${1}.log
+cd -
+
+rm -rf $1
