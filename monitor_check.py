a354870957d549c99e0c4627a4b8bb1814a9da07
Miro Hrončok <miro@hroncok.cz>
2020-02-27 20:52:18 +0100

    Update the bug text to a4

--- monitor_check.py
+++ monitor_check.py
@@ -226,7 +226,7 @@ async def open_bz(package, build, status, browser_lock):
     summary = f"{package} fails to build with Python 3.9"
 
     description = dedent(f"""
-        {package} fails to build with Python 3.9.0a3.
+        {package} fails to build with Python 3.9.0a4.
 
         This report is automated and not very verbose, but we'll try to get back here with details.
 
