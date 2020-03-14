5b4b38b0a4f46295d36d6e494f536d179e1ef93a
Miro Hrončok <miro@hroncok.cz>
2020-03-14 02:29:08 +0100

    pcp filed in setools as bz1813076

--- monitor_check.py
+++ monitor_check.py
@@ -43,6 +43,7 @@ EXCLUDE = {
     'python-certbot-dns-rfc2136': 'filed in python-dns as bz1801247',
     'python-mutatormath': 'filed in fonttools as bz1809062',
     'asv': 'filed in feedparser as bz1807186',
+    'pcp': 'filed in setools as bz1813076',
 }
 
 # Packages failing for root.log issues with long build.logs
