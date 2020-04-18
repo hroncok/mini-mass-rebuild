95f4ac0757fcdd2616bebd4bbad64c6b1b4d89e6
Miro Hrončok <miro@hroncok.cz>
2020-04-18 12:24:47 +0200

    Use rpm.labelCompare instead of subprocess to make things superfast

--- repo_findold.py
+++ repo_findold.py
@@ -1,28 +1,40 @@
-import asyncio
-import os
 import pathlib
-import sys
+import rpm
+
+SIGNS = {
+    1: '>',
+    0: '==',
+    -1: '<',
+}
 
 
-async def main():
+def split(nevra):
+    nev, _, ra = nevra.rpartition('-')
+    n, _, ev = nev.rpartition('-')
+    e, _, v = ev.rpartition(':')
+    r, _, a = ra.rpartition('.')
+    return n, (e, v, r)
+
+
+def main():
     python38 = set(pathlib.Path('python38.pkgs').read_text().splitlines())
 
     kojirepo = set(pathlib.Path('koji.repoquery').read_text().splitlines())
     py39repo = set(pathlib.Path('python39.repoquery').read_text().splitlines())
 
-    kojidict = {pkg.rsplit('-', 2)[0]: "{0}-{1}".format(*pkg.rsplit('-', 2)[1:]) for pkg in kojirepo}
-    py39dict = {pkg.rsplit('-', 2)[0]: "{0}-{1}".format(*pkg.rsplit('-', 2)[1:]) for pkg in py39repo}
+    kojidict = dict(split(pkg) for pkg in kojirepo)
+    py39dict = dict(split(pkg) for pkg in py39repo)
 
     todo = set()
-    tasks = []
-
-    semaphore = asyncio.Semaphore(os.cpu_count())
 
     for pkg in sorted(python38):
         if pkg not in py39dict:
             continue
-        tasks.append(compare(pkg, kojidict[pkg], py39dict[pkg], todo, semaphore))
-    await asyncio.gather(*tasks)
+        sign = SIGNS[rpm.labelCompare(kojidict[pkg], py39dict[pkg])]
+        print(f'{pkg: <30} {"-".join(kojidict[pkg])} {sign} {"-".join(py39dict[pkg])}')
+
+        if sign == '>':
+            todo.add(pkg)
 
     print()
 
@@ -30,22 +42,5 @@ async def main():
         print(pkg)
 
 
-async def compare(pkg, kojiver, py39ver, todo, semaphore):
-    cmd = ('rpmdev-vercmp', kojiver, py39ver)
-    async with semaphore:
-        proc = await asyncio.create_subprocess_exec(*cmd,
-                                                    stdout=asyncio.subprocess.PIPE,
-                                                    stderr=asyncio.subprocess.PIPE)
-
-        stdout, stderr = await proc.communicate()
-
-    print(f'{pkg: <30} {stdout.decode().strip()}')
-    if stderr:
-        print(stderr.decode().strip(), file=sys.stderr)
-
-    if proc.returncode == 11:
-        todo.add(pkg)
-
-
 if __name__ == '__main__':
-    asyncio.run(main())
+    main()
