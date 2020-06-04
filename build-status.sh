eeeded18413cba89bb73a0063e07fd60b7c0e15a
Miro Hrončok <miro@hroncok.cz>
2020-06-04 19:23:50 +0200

    Add a longer timeout to build-status.sh

--- build-status.sh
+++ build-status.sh
@@ -1,1 +1,1 @@
-echo $1 $(http $(grep 'Task info' $1.log | cut -d' ' -f3) | grep '<td class="task' | cut -d'>' -f2)
+echo $1 $(http --timeout 300 $(grep 'Task info' $1.log | cut -d' ' -f3) | grep '<td class="task' | cut -d'>' -f2)
