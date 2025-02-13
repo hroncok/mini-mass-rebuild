51c22c9faaf6cca23979183d6f4fa264665cda7f
Karolina Surma <ksurma@redhat.com>
2025-02-13 13:05:32 +0100

    We build with a5 now

--- monitor_check.py
+++ monitor_check.py
@@ -572,7 +572,7 @@ async def open_bz(package, build, status, browser_lock, reason=None):
     summary = f"{package} fails to build with Python 3.14: {reason['short_description']}"
 
     description = dedent(f"""
-        {package} fails to build with Python 3.14.0a2.
+        {package} fails to build with Python 3.14.0a5.
 
         {reason['long_description']}
 
