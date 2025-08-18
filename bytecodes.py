9c5562867f7ed9d3a6e698a489433467e8d0990f
Miro Hrončok <miro@hroncok.cz>
2025-08-18 13:47:04 +0200

    Update bytecodes.py for rc2

--- bytecodes.py
+++ bytecodes.py
@@ -2,7 +2,7 @@ import json
 import subprocess
 from click import progressbar
 
-repoquery = 'repoquery --repo=koji --refresh -f *.cpython-314.pyc --source'.split()
+repoquery = 'repoquery --repo=koji43 --refresh -f *.cpython-314.pyc --source'.split()
 py314_pkgs = subprocess.run(repoquery, stdout=subprocess.PIPE, text=True).stdout.splitlines()
 
 try:
@@ -25,6 +25,15 @@ def isf(item):
     return f'[+{len(done)}/{len(inspection)}/-{len(torebuild)}] {item}'
 
 
+def built_by(nevr, username):
+    cmd = ('koji', 'buildinfo', nevr)
+    lines = subprocess.run(cmd, stdout=subprocess.PIPE, text=True).stdout.splitlines()
+    for line in lines:
+        if line.startswith('Built by: '):
+            return line.strip() == f'Built by: {username}'
+    raise RuntimeError('this should never happen')
+
+
 try:
     with progressbar(py314_pkgs, item_show_func=isf) as bar:
         for pkg in bar:
@@ -32,10 +41,13 @@ try:
             name = '-'.join(nevr.split('-')[:-2])
             if name in processed:
                 continue
-            if nevr not in after(name, '2025-06-18 23:59:59'):
-                # https://koji.fedoraproject.org/koji/buildinfo?buildID=2733872
-                if nevr not in after(name, '2025-06-18 13:22:48'):
+            if nevr not in after(name, '2025-08-15 23:59:59'):
+                # https://koji.fedoraproject.org/koji/buildinfo?buildID=2791425
+                # https://bodhi.fedoraproject.org/updates/FEDORA-2025-2fe07c73d5
+                if nevr not in after(name, '2025-08-15 12:47:23'):
                     torebuild.add(name)
+                elif built_by(nevr, 'churchyard'):
+                    done.add(name)
                 else:
                     inspection.add(name)
             else:
